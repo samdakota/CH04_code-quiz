4842b57a66afa00169d1e7c0291b93a252e644e4
Sam Miller <sam.dakota@gmail.com>
2022-03-20 23:50:32 -0400

    final updates

--- assets/script.js
+++ assets/script.js
@@ -20,15 +20,30 @@
 // question objects?
 const questions = [
   {
-    question: "which number is 1?",
-    choices: ["1", "2", "3", "4"],
-    answer: "1",
+    question: "How do you create a flexbox?",
+    choices: ["display: flex", "display: flexbox", "display: box", "flex: display"],
+    answer: "display: flex",
   },
   {
-    question: "which number is 3?",
-    choices: ["1", "2", "3", "4"],
-    answer: "3",
+    question: "Which method saves data to localStorage?",
+    choices: ["getItem()", "saveItem", "pushItem", "setItem"],
+    answer: "setItem",
   },
+  {
+    question: "Which command is used to change the git branch?",
+    choices: ["git add .", "git checkout", "git checkout -b", "git change"],
+    answer: "git checkout -b",
+  },
+  {
+    question: "What JavaScript feature would you use to execute a block of code a certain number of times?",
+    choices: ["a for loop", "an object", "an array", "a console.log"],
+    answer: "a for loop",
+  },
+  {
+    question: "What is the index value of the first object in an array?",
+    choices: ["0", "1", "-1", "undefined"],
+    answer: "0",
+  }
 ];
 
 var indexCount = 0;
@@ -45,7 +60,9 @@ var startButton = document.getElementById("btn-start");
 var endButton = document.getElementById("btn-end");
 var timeLeft = document.getElementById("time-left");
 
-var nameInput = document.querySelector("input[name='initials']").value;
+var savedScores = [];
+var savedScoresIdCounter = 0;
+
 
 var startClock = function () {
   timer = setInterval(function () {
@@ -118,11 +135,17 @@ var endQuiz = function () {
 };
 
 var saveScore = function () {
+  var nameInput = document.querySelector("input[name='initials']").value;
   var savedScoreList = document.getElementById("saved-scores");
   var newScore = document.createElement("li");
-  newScore.textContent = nameInput;
+  newScore.textContent = nameInput + "   :   " + score;
   savedScoreList.appendChild(newScore);
-  console.log(newScore);
+  var savedScoreObj = {
+    name: nameInput,
+    value: score,
+    id: savedScoresIdCounter++,
+  }
+  savedScores.push(savedScoreObj);
 };
 
 endButton.addEventListener("click", saveScore);
